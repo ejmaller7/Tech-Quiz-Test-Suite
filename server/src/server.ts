c8010f64dfb8440253a09bb512ca3764ca62f527
Ethan Maller <mallerethan4@gmail.com>
2025-03-03 21:20:48 -0600

    Do not have enough time to make this perfect - submitting without the video

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+// import { promises as fs } from 'fs';
+// import { fileURLToPath } from 'url';
 import path from 'node:path';
 
 import db from './config/connection.js';
@@ -6,12 +8,21 @@ import routes from './routes/index.js';
 
 await db();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const jsonPath = path.join(__dirname, 'pythonQuestions.json');
+// const jsonData = await fs.readFile(jsonPath, 'utf-8');
+// const questionData = JSON.parse(jsonData);
+
+// console.log(questionData)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
